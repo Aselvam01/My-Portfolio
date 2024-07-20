7b3b31e1c61b7dd598486bfc6383c380d82ad7e9
SELVAM A <144670858+Aselvam01@users.noreply.github.com>
2024-07-20 19:44:19 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+    // Implement actual form submission logic here
+});
